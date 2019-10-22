5acb20c33d7bd0825fb090e2a5e6fcce5cedef8f
Maarten Verburg <maarten.verburg@joorz.nl>
2019-10-22 10:19:23 +0200

    add helper script to create epub

---
+++ create-epub.sh
@@ -0,0 +1,1 @@
+pandoc --resource-path=.:images -t epub3 -o git-from-the-bottom-up.epub meta.yml index.md 1-Repository/*.md 2-The-Index/*.md 3-Reset/*.md 4-Stashing-and-the-reflog.md 5-Conclusion.md 6-Further-Reading.md
